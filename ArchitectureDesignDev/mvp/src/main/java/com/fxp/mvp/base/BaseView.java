bb7cbf2c1ff5ca4fb7a262266d9162f4f5f68d39
fangxiaopeng <850899969@qq.com>
2018-11-08 16:30:38 +0800

    提交BaseView.java

---
+++ ArchitectureDesignDev/mvp/src/main/java/com/fxp/mvp/base/BaseView.java
@@ -0,0 +1,24 @@
+package com.fxp.mvp.base;
+
+/**
+ * Title:       BaseView
+ * <p>
+ * Package:     com.fxp.mvp.base
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/11/7 8:27 PM
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/11/7    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public interface BaseView {
+}
