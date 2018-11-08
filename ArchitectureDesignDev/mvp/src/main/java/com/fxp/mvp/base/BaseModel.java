13302c923f49070a62c110dca5691c0b3bdb4112
fangxiaopeng <850899969@qq.com>
2018-11-08 16:29:42 +0800

    提交BaseModel.java

---
+++ ArchitectureDesignDev/mvp/src/main/java/com/fxp/mvp/base/BaseModel.java
@@ -0,0 +1,24 @@
+package com.fxp.mvp.base;
+
+/**
+ * Title:       BaseModel
+ * <p>
+ * Package:     com.fxp.mvp.base
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/11/7 8:25 PM
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/11/7    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public interface BaseModel {
+}
