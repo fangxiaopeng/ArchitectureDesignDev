395b6731f6b3169777d2821856c72672a1764775
fangxiaopeng <850899969@qq.com>
2018-11-16 14:42:45 +0800

    提交DynamicPublishPresenterImpl.java

---
+++ ArchitectureDesignDev/mvp/src/main/java/com/fxp/mvp/presenter/impl/DynamicPublishPresenterImpl.java
@@ -0,0 +1,27 @@
+package com.fxp.mvp.presenter.impl;
+
+/**
+ * Title:       DynamicPublishPresenterImpl
+ * <p>
+ * Package:     com.fxp.mvp.presenter.impl
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/11/15 6:52 PM
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/11/15    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public interface DynamicPublishPresenterImpl {
+
+    void requestData(int pageIndex, int pageSize);
+
+}
