e47dc1689fc3a8abf79ad82069bcbfb43e8a00e4
fangxiaopeng <850899969@qq.com>
2018-11-16 14:40:50 +0800

    提交DynamicBean

---
+++ ArchitectureDesignDev/mvp/src/main/java/com/fxp/mvp/entity/DynamicBean.java
@@ -0,0 +1,24 @@
+package com.fxp.mvp.entity;
+
+/**
+ * Title:       DynamicBean
+ * <p>
+ * Package:     com.fxp.mvp.entity
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/11/16 10:31 AM
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/11/16    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public class DynamicBean {
+}
