85611f73f08d15ad5eb799814997f1e8767fc738
fangxiaopeng <850899969@qq.com>
2018-11-16 14:41:39 +0800

    DynamicPublishModelImpl.java

---
+++ ArchitectureDesignDev/mvp/src/main/java/com/fxp/mvp/model/impl/DynamicPublishModelImpl.java
@@ -0,0 +1,24 @@
+package com.fxp.mvp.model.impl;
+
+/**
+ * Title:       DynamicPublishModelImpl
+ * <p>
+ * Package:     com.fxp.mvp.model.impl
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/11/15 6:54 PM
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/11/15    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public interface DynamicPublishModelImpl {
+}
