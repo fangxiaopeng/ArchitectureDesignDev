a8d5aed55856479359e2623394f5cd6559440308
fangxiaopeng <850899969@qq.com>
2018-11-17 18:14:17 +0800

    提交Constants.java

---
+++ ArchitectureDesignDev/mvp/src/main/java/com/fxp/mvp/utils/Constants.java
@@ -0,0 +1,29 @@
+package com.fxp.mvp.utils;
+
+/**
+ * Title:       Constants
+ * <p>
+ * Package:     com.fxp.mvp.utils
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/11/16 3:45 PM
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/11/16    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public class Constants {
+
+    public static final int TYPE_STATE = 0;
+
+    public static final int TYPE_BLOG = 1;
+
+}
