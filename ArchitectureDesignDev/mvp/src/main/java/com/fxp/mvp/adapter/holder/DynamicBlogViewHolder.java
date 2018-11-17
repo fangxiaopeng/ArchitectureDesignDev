29318ea16def6510b798d538c6ff36ca9f183468
fangxiaopeng <850899969@qq.com>
2018-11-17 18:15:57 +0800

    提交DynamicBlogViewHolder.java

---
+++ ArchitectureDesignDev/mvp/src/main/java/com/fxp/mvp/adapter/holder/DynamicBlogViewHolder.java
@@ -0,0 +1,32 @@
+package com.fxp.mvp.adapter.holder;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Title:       DynamicBlogViewHolder
+ * <p>
+ * Package:     com.fxp.mvp.adapter.holder
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/11/16 3:37 PM
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/11/16    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public class DynamicBlogViewHolder extends BasicViewHolder<String> {
+
+    public DynamicBlogViewHolder(Context context, View itemView) {
+        super(itemView);
+    }
+
+}
