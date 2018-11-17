a813ac97c68c6c0ea8b14af9e83044576966d80d
fangxiaopeng <850899969@qq.com>
2018-11-17 18:15:32 +0800

    提交BasicViewHolder.java

---
+++ ArchitectureDesignDev/mvp/src/main/java/com/fxp/mvp/adapter/holder/BasicViewHolder.java
@@ -0,0 +1,33 @@
+package com.fxp.mvp.adapter.holder;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Title:       BasicViewHolder
+ * <p>
+ * Package:     com.fxp.mvp.adapter.holder
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/11/16 3:14 PM
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/11/16    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public abstract class BasicViewHolder<T> extends RecyclerView.ViewHolder {
+
+    public BasicViewHolder(View itemView) {
+        super(itemView);
+    }
+
+
+}
