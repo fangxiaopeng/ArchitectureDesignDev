601854315ca6e2a68e9b9f27be37eafea5323173
fangxiaopeng <850899969@qq.com>
2018-11-17 18:14:52 +0800

    提交DynamicPublishFragment.java

---
+++ ArchitectureDesignDev/mvp/src/main/java/com/fxp/mvp/ui/fragment/DynamicPublishFragment.java
@@ -0,0 +1,24 @@
+package com.fxp.mvp.ui.fragment;
+
+/**
+ * Title:       DynamicPublishFragment
+ * <p>
+ * Package:     com.fxp.mvp.ui.fragment
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/11/16 4:01 PM
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/11/16    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public class DynamicPublishFragment {
+}
