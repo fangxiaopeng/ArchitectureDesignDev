00d2cb5ef6fd4c92364a1a2534e01e21539249ff
fangxiaopeng <850899969@qq.com>
2018-11-17 18:16:18 +0800

    提交DynamicStateViewHolder.java

---
+++ ArchitectureDesignDev/mvp/src/main/java/com/fxp/mvp/adapter/holder/DynamicStateViewHolder.java
@@ -0,0 +1,32 @@
+package com.fxp.mvp.adapter.holder;
+
+import android.content.Context;
+import android.view.View;
+
+/**
+ * Title:       DynamicStateViewHolder
+ * <p>
+ * Package:     com.fxp.mvp.adapter.holder
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2018/11/16 3:36 PM
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2018/11/16    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public class DynamicStateViewHolder extends BasicViewHolder<String> {
+
+    public DynamicStateViewHolder(Context context, View itemView) {
+        super(itemView);
+    }
+
+}
