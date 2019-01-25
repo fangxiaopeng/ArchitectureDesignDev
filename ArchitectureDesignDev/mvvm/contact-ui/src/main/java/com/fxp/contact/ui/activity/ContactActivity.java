856808ee6d017b3fdafd46203ce577b070227bf9
星火方晓鹏 <fangxiaopeng@126.com>
2019-01-25 10:27:08 +0800

    commit ContactActivity

---
+++ ArchitectureDesignDev/mvvm/contact-ui/src/main/java/com/fxp/contact/ui/activity/ContactActivity.java
@@ -0,0 +1,36 @@
+package com.fxp.contact.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.fxp.contact.ui.R;
+
+/**
+ * Title:       ContactActivity
+ * <p>
+ * Package:     com.fxp.contact.ui.activity
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2019/1/24 8:23 PM
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2019/1/24    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public class ContactActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+    }
+
+}
