d443dde9d13a623907e2aba9cd90797e2dfc8f7e
fangxiaopeng <850899969@qq.com>
2019-02-03 17:32:31 +0800

    Create IContactNavigator.java

---
+++ ArchitectureDesignDev/mvvm/contact-core/src/main/java/com/fxp/contact/core/mvvm/view/IContactNavigator.java
@@ -0,0 +1,41 @@
+package com.fxp.contact.core.mvvm.view;
+
+import com.fxp.contact.core.base.IBaseView;
+
+/**
+ * Title:       IContactNavigator
+ * <p>
+ * Package:     com.fxp.contact.core.mvvm.view
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2019/2/2 9:43 PM
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2019/2/2    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public class IContactNavigator implements IBaseView {
+
+    @Override
+    public void loadStart(int loadType) {
+
+    }
+
+    @Override
+    public void loadComplete() {
+
+    }
+
+    @Override
+    public void loadFailure(String message) {
+
+    }
+}
