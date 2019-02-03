e1a0edcb3bfa81a69c2fa7a226403cb99be5f1a8
fangxiaopeng <850899969@qq.com>
2019-02-03 17:32:36 +0800

    Create ContactModel.java

---
+++ ArchitectureDesignDev/mvvm/contact-core/src/main/java/com/fxp/contact/core/mvvm/model/ContactModel.java
@@ -0,0 +1,32 @@
+package com.fxp.contact.core.mvvm.model;
+
+import com.fxp.contact.core.base.BaseLoadListener;
+import com.fxp.contact.core.entity.ContactInfo;
+
+/**
+ * Title:       ContactModel
+ * <p>
+ * Package:     com.fxp.contact.core.mvvm.model
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2019/2/2 9:41 PM
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2019/2/2    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public class ContactModel implements IContactModel {
+
+    @Override
+    public void loadData(int page, BaseLoadListener<ContactInfo> loadListener) {
+
+    }
+}
