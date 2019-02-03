b9c19ef02625e2d7cb1b93f9132462ef62556756
fangxiaopeng <850899969@qq.com>
2019-02-03 17:32:43 +0800

    Create ContactVM.java

---
+++ ArchitectureDesignDev/mvvm/contact-core/src/main/java/com/fxp/contact/core/mvvm/viewmodel/ContactVM.java
@@ -0,0 +1,51 @@
+package com.fxp.contact.core.mvvm.viewmodel;
+
+import com.fxp.contact.core.base.BaseLoadListener;
+import com.fxp.contact.core.entity.ContactInfo;
+
+import java.util.List;
+
+/**
+ * Title:       ContactVM
+ * <p>
+ * Package:     com.fxp.contact.core.mvvm.viewmodel
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2019/2/2 9:45 PM
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2019/2/2    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public class ContactVM implements BaseLoadListener<ContactInfo> {
+
+    public ContactVM(){}
+
+    @Override
+    public void loadSuccess(List<ContactInfo> list) {
+
+    }
+
+    @Override
+    public void loadFailure(String message) {
+
+    }
+
+    @Override
+    public void loadStart() {
+
+    }
+
+    @Override
+    public void loadComplete() {
+
+    }
+}
