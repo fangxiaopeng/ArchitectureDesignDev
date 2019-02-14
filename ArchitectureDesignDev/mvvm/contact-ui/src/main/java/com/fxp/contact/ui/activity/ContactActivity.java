2b231e2f974d54ea5b2dcb3422da6428e3c12a83
fangxiaopeng <850899969@qq.com>
2019-02-14 16:20:08 +0800

    Update ContactActivity.java

--- ArchitectureDesignDev/mvvm/contact-ui/src/main/java/com/fxp/contact/ui/activity/ContactActivity.java
+++ ArchitectureDesignDev/mvvm/contact-ui/src/main/java/com/fxp/contact/ui/activity/ContactActivity.java
@@ -3,6 +3,7 @@ package com.fxp.contact.ui.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.fxp.contact.core.mvvm.view.IContactNavigator;
 import com.fxp.contact.core.mvvm.viewmodel.ContactVM;
 import com.fxp.contact.ui.R;
 import com.fxp.contact.ui.databinding.ActivityContactBinding;
@@ -27,7 +28,7 @@ import com.fxp.contact.ui.databinding.ActivityContactBinding;
  * <p>
  * Github:  https://github.com/fangxiaopeng
  */
-public class ContactActivity extends BaseActivity<ContactVM, ActivityContactBinding> {
+public class ContactActivity extends BaseActivity<ContactVM, ActivityContactBinding> implements IContactNavigator {
 
     @Override
     int getLayoutId() {
@@ -42,6 +43,21 @@ public class ContactActivity extends BaseActivity<ContactVM, ActivityContactBind
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public void loadStart(int loadType) {
+
+    }
+
+    @Override
+    public void loadComplete() {
+
+    }
+
+    @Override
+    public void loadFailure(String message) {
 
     }
 }
