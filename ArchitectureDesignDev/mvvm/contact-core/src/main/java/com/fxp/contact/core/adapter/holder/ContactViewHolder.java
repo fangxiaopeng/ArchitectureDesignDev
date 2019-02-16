57e4eaecf641d008e3595e5a81fc2856c797e73b
fangxiaopeng <850899969@qq.com>
2019-02-16 22:21:12 +0800

    Create ContactViewHolder.java

---
+++ ArchitectureDesignDev/mvvm/contact-core/src/main/java/com/fxp/contact/core/adapter/holder/ContactViewHolder.java
@@ -0,0 +1,34 @@
+package com.fxp.contact.core.adapter.holder;
+
+import android.databinding.ViewDataBinding;
+
+import com.fxp.contact.core.base.BaseViewHolder;
+
+/**
+ * Title:       ContactViewHolder
+ * <p>
+ * Package:     com.fxp.contact.core.adapter.holder
+ * <p>
+ * Author:      fxp
+ * <p>
+ * Create at:   2019/2/14 4:47 PM
+ * <p>
+ * Description:
+ * <p>
+ * <p>
+ * Modification History:
+ * <p>
+ * Date       Author       Version      Description
+ * -----------------------------------------------------------------
+ * 2019/2/14    fxp       1.0         First Created
+ * <p>
+ * Github:  https://github.com/fangxiaopeng
+ */
+public class ContactViewHolder extends BaseViewHolder<ViewDataBinding>{
+
+    public ContactViewHolder(ViewDataBinding binding) {
+        super(binding);
+    }
+
+
+}
